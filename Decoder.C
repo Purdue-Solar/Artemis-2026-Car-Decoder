5827ed94645d0eddc350af6ec922156c6f946200
Nanson Chen <buyingsaucer2@gmail.com>
2026-01-27 20:35:21 -0500

    Code style and argument checking

--- Decoder.C
+++ Decoder.C
@@ -1,4 +1,10 @@
+/* Authors: Nanson Chen, Daniel Xu */
+/* Created on: 1/24/2026 */
+#include "decoder.h"
+
 #include <stdint.h>
+#include <stdio.h>
+
 struct message {
     uint32_t time_stamp;
     uint8_t battery_temp;
@@ -12,3 +18,28 @@ struct message {
     uint16_t temp;
     uint16_t oh_no_bits;
 };
+
+/*
+ * Function Name: main
+ * The entry point of the decoder.
+ * 
+ * Returns: An exit code
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Expected 1 argument, gotten %d\n", (argc - 1));
+        printf("Arguments read:\n");
+        for (int i = 1; i < argc; i++) {
+            printf("%s", argv[i]);
+            if (i != argc - 1) {
+                printf(", ");
+            }
+        }
+        printf("\n");
+        printf("Usage: <executable> <output_path>\n");
+        return 1;
+    }
+
+    printf("This code is chopped\n");
+    return 0;
+}
---
+++ decoder.h
@@ -0,0 +1,6 @@
+#ifndef DECODER_H
+#define DECODER_H
+
+int main(int argc, char *argv[]);
+
+#endif /* DECODER_H */
