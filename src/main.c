3d77a02c19fea21d48ebcc230f0792878e9b2a5e
DeLobster <xudaniel1025@gmail.com>
2026-03-07 11:57:06 -0500

    changed the main function after the removed buffer

--- src/main.c
+++ src/main.c
@@ -1,30 +1,15 @@
 /* main.c */
 /* Authors: Daniel Xu */
 #include <stdio.h>
-#include "buffer.h"
+#include <stdint.h>
 #include "can_decode.h"
 
-#define BUFFER_SIZE_BYTES (8u * 1024u * 1024u)  // 8 MB
-
 int main(void) {
-    Buffer buf;
-    if (buffer_init(&buf, BUFFER_SIZE_BYTES) != 0) {
-        fprintf(stderr, "Failed to init buffer\n");
-        return 1;
-    }
-
     uint8_t incoming[2] = { 0xA5, 0x96 };
-    buffer_write(&buf, incoming, 2);
 
-    Row11Flags flags;
-    if (decode_row11_from_buffer(&buf, &flags) != 0) {
-        fprintf(stderr, "Not enough data to decode\n");
-        buffer_free(&buf);
-        return 1;
-    }
+    Row11Flags flags = decode_row11_from_bytes(incoming[0], incoming[1]);
 
     print_row11(&flags);
 
-    buffer_free(&buf);
     return 0;
 }
